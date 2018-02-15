942c76b187e0b506f2677f69fd7fc9ecf6eb60af
Kontra <rofleboon@gmail.com>
2018-02-15 15:23:41 +0100

    Add project files.

---
+++ revision_data.h
@@ -0,0 +1,17 @@
+#ifndef __REVISION_DATA_H__
+#define __REVISION_DATA_H__
+ #define _HASH                      "4da9321d2bdd"
+ #define _DATE                      "2018-02-15 12:23:05 +0100"
+ #define _BRANCH                    "3.3.5"
+ #define _CMAKE_COMMAND             R"(C:/Program Files/CMake/bin/cmake.exe)"
+ #define _SOURCE_DIRECTORY          R"(D:/Kontra/Core)"
+ #define _BUILD_DIRECTORY           R"(D:/Kontra/Build)"
+ #define _MYSQL_EXECUTABLE          R"(C:/Program Files/MySQL/MySQL Server 5.5/bin/mysql.exe)"
+ #define _FULL_DATABASE             "TDB_full_world_335.63_2017_04_18.sql"
+ #define VER_COMPANYNAME_STR        "TrinityCore Developers"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2017 TrinityCore"
+ #define VER_FILEVERSION            0,0,0
+ #define VER_FILEVERSION_STR        "4da9321d2bdd 2018-02-15 12:23:05 +0100 (3.3.5 branch)"
+ #define VER_PRODUCTVERSION         VER_FILEVERSION
+ #define VER_PRODUCTVERSION_STR     VER_FILEVERSION_STR
+#endif // __REVISION_DATA_H__
